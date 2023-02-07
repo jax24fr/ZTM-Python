69ae974fe521171d3887fbdab26be75ee28bf7f0
Jax24fr <63259266+jax24fr@users.noreply.github.com>
2023-02-07 16:45:16 +0000

    Formatted Strings

--- main.py
+++ main.py
@@ -10,3 +10,17 @@
 
 print(round(3.9))
 print(abs(-20))
+
+#Escape Sequence
+
+weather = "It's kind of sunny \nhope you have a good day!"
+print(weather)
+
+#formatted strings
+name = "johnny"
+age = 55
+
+print ("hi " + name + " you are " + str(age) + " years old")
+print (f"hi {name} you are {age} years old")
+print("hi {} you are {} years old".format(name, age))
+print("hi {1} you are {0} years old".format(name, age))
